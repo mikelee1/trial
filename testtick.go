af144c36d5700fbffc70c3e9de48f20a1742b2ac
mike <liyy@yunphant.com>
2019-07-28 10:52:13 +0800

    add tick demo

---
+++ testtick.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	ticker := time.NewTicker(2*time.Second)
+	defer ticker.Stop()
+	for {
+		time.Sleep(time.Second)
+		select {
+		case t := <-ticker.C:
+			fmt.Println(t)
+		default:
+			fmt.Println("default")
+
+		}
+	}
+}
