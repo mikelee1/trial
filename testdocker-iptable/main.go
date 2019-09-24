afd56a068a884e903bda956baa6a054027b8f2de
mike <liyy@yunphant.com>
2019-09-24 20:31:37 +0800

    update

--- testdocker-iptable/main.go
+++ testdocker-iptable/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/fsouza/go-dockerclient"
-	logger2 "myproj/try/common/logger"
 	"github.com/op/go-logging"
+	logger2 "myproj/try/common/logger"
 )
 
 var host = "http://192.168.9.67:2375"
 var logger *logging.Logger
 var dockerClient *docker.Client
+
 func init() {
 	logger = logger2.GetLogger()
 	dockerClient, _ = docker.NewClient(host)
@@ -20,14 +21,14 @@ func main() {
 	container, err := dockerClient.CreateContainer(docker.CreateContainerOptions{
 		Name: containerName,
 		Config: &docker.Config{
-			Image: "alpine-ipts",
+			Image: "alpine-ipts:latest",
 			//./start.sh --rm -ti --cap-add=NET_ADMIN
-			//Cmd:[]string{"./start.sh","--rm","-ti","--cap-add=NET_ADMIN"},
+			//Cmd: []string{"sh", "-c", "sh"},
+			Cmd:        []string{"sh", "-c", "./start.sh && sh"},
 			WorkingDir: "/home",
-			Cmd: []string{"sh", "-c", "sh"},
-			Tty: true,
-			ExposedPorts:map[docker.Port]struct{}{
-				"8882": struct {}{},
+			Tty:        true,
+			ExposedPorts: map[docker.Port]struct{}{
+				"8882": struct{}{},
 			},
 		},
 		HostConfig: &docker.HostConfig{
@@ -35,19 +36,17 @@ func main() {
 				//mike 宿主机上的地址进行映射
 				"/home/ubuntu/start.sh:/home/start.sh:rw",
 			},
-			Privileged:true,
+			Privileged: true,
 			UsernsMode: "host",
-			PortBindings:map[docker.Port][]docker.PortBinding{
-				"8882":[]docker.PortBinding{
+			PortBindings: map[docker.Port][]docker.PortBinding{
+				"8882": []docker.PortBinding{
 					docker.PortBinding{
-						HostIP:"0.0.0.0",
-						HostPort:"18882",
+						HostIP:   "0.0.0.0",
+						HostPort: "18882",
 					},
 				},
 			},
-
 		},
-
 	})
 	if err != nil {
 		logger.Error(err)
@@ -61,7 +60,7 @@ func main() {
 
 }
 
-func RemoveContainer(containerName string)  {
+func RemoveContainer(containerName string) {
 	//获取容器列表
 	conlist, err := dockerClient.ListContainers(docker.ListContainersOptions{
 		All: true,
@@ -76,6 +75,7 @@ func RemoveContainer(containerName string)  {
 		logger.Info(value.Names[0])
 		//删除test容器
 		if value.Names[0] == "/"+containerName {
+			dockerClient.InspectContainer(value.ID)
 			err = dockerClient.RemoveContainer(docker.RemoveContainerOptions{
 				Force: true,
 				ID:    value.ID,
