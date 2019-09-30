a4884fb600ed27fc7061a4510052c4a2938789da
mike <liyy@yunphant.com>
2019-09-30 14:30:29 +0800

    update dockeriptables

--- testdocker-iptable/start.sh
+++ testdocker-iptable/start.sh
@@ -3,12 +3,12 @@
 #testalpine2 172.22.0.2
 #testalpine3 172.22.0.3 wget 192.168.9.87 9991
 
-
-#echo `ip a show eth0|grep inet|cut -d/ -f1|awk '{print $2}'`
-
-iptables -t nat -A PREROUTING -d 172.17.0.9 -p tcp --dport 8882 -j DNAT --to-destination 192.168.9.87:9992
-
-iptables -t nat -A POSTROUTING -d 192.168.9.87 -p tcp --dport 9992 -j SNAT --to 172.17.0.9
+#获取ipv4的地址
+IPADDR=`ip a show eth0|grep inet|cut -d/ -f1|head -1|awk '{print $2}'`
+#从8882转出到9992
+iptables -t nat -A PREROUTING -d $IPADDR -p tcp --dport 8882 -j DNAT --to-destination 192.168.9.87:9992
+#从9992转回到8882
+iptables -t nat -A POSTROUTING -d 192.168.9.87 -p tcp --dport 9992 -j SNAT --to $IPADDR
 
 exec syslogd -n -O -
 
