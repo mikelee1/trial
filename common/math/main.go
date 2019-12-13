cd84fecd54e3f0aaa85a5465bb59a1731b7e635c
mike <liyy@yunphant.com>
2019-12-13 22:16:14 +0800

    float转为int

---
+++ common/math/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(RoundViaFloat(0.301,2))
+}
+
+func RoundViaFloat(x float64, prec int) float64 {
+	var rounder float64
+	pow := math.Pow(10, float64(prec))
+	intermed := x * pow
+	_, frac := math.Modf(intermed)
+	intermed += .5
+	x = .5
+	if frac < 0.0 {
+		x=-.5
+		intermed -=1
+	}
+	if frac >= x {
+		rounder = math.Ceil(intermed)
+	} else {
+		rounder = math.Floor(intermed)
+	}
+
+	return rounder / pow
+}
