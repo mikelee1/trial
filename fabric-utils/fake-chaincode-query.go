a93c49e142b17bed584a92a18ff2ef7b2ce88fa9
mike <liyy@yunphant.com>
2020-07-12 11:43:29 +0800

    添加fabric调用

---
+++ fabric-utils/fake-chaincode-query.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"myproj/try/common/execshell"
+	"sync"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	count := 2
+	wg.Add(count)
+	for i := 0; i < count; i++ {
+		go func() {
+			defer wg.Done()
+			//str, err := Run(`peer chaincode query -C mychannel -n mycc -c '{"Args":["query","a"]}'`)
+			str, err := execshell.Run(`peer chaincode query -C mychannel -n mycc1 -c '{"Args":["checkMemberByFullName","{\"custFullName\":\"kjsdf\"}"]}'`)
+			if err != nil {
+				fmt.Println(str, err)
+			}
+
+		}()
+	}
+	wg.Wait()
+}
